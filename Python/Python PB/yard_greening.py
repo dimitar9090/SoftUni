0e8b5f1fd2da5871f113d5d62c73b59ba3b7e780
Dimitar Anguelov <113285865+dimitar9090@users.noreply.github.com>
2023-04-24 23:57:13 +0300

    Delete yard_greening.py

--- Python/Python PB/yard_greening.py
+++
@@ -1,6 +0,0 @@
-yard = float(input())
-first_price = yard * 7.61
-discount = 0.18 * first_price
-final_price = first_price - discount
-print(f"The final price is: {final_price} lv.")
-print(f"The discount is: {discount} lv.")
