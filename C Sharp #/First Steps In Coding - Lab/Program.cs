41ab21022399afcc63ca0398009773e8672224af
Dimitar Anguelov <113285865+dimitar9090@users.noreply.github.com>
2023-04-24 15:44:23 +0300

    Add files via upload

---
+++ C Sharp #/First Steps In Coding - Lab/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _07.ProjectsCreation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int numberOfProjects = int.Parse(Console.ReadLine());
+            int hourNeeded = numberOfProjects * 3;
+            Console.WriteLine($"The architect {name} will need {hourNeeded} hours to complete {numberOfProjects} project/s.");
+        }
+    }
+}
