54f06638e0c3b02aa40b92c2974f6eb12b329d3a
Dimitar Anguelov <d.angelov9090@gmail.com>
2023-05-04 00:26:19 +0300

    conditional statements

---
+++ C Sharp #/Conditional Statements - Lab/04.PasswordGuess/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04.PasswordGuess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input_password = Console.ReadLine();
+            if (input_password == "s3cr3t!P@ssw0rd")
+            {
+                Console.WriteLine("Welcome");
+
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+
+
+        }
+    }
+}
