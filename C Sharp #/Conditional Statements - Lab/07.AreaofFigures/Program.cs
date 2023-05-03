5b315ddabacb6655c4f85f2de8e2dffa9190b757
Dimitar Anguelov <d.angelov9090@gmail.com>
2023-05-04 02:08:06 +0300

    finish con.stat. lab

---
+++ C Sharp #/Conditional Statements - Lab/07.AreaofFigures/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _07.AreaofFigures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+            
+
+            if (figure == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double area = side * side;
+                Console.WriteLine("{0:F3}", area);
+            }
+            else if (figure == "rectangle")
+            {
+                double side1 = double.Parse((Console.ReadLine()));
+                double side2 = double.Parse((Console.ReadLine()));
+                double area = side1 * side2;
+                Console.WriteLine("{0:F3}", area);
+            }
+            else if (figure == "circle")
+            {
+                double diameter = double.Parse((Console.ReadLine()));
+                double area = diameter * diameter * Math.PI;
+                Console.WriteLine("{0:F3}", area);
+            }
+            else if (figure == "triangle")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double high = double.Parse(Console.ReadLine());
+                double area = side * high / 2;
+                Console.WriteLine("{0:F3}", area);
+            }
+            
+        }
+    }
+}
